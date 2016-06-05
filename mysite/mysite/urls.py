331b23b6f903d124ebb901471f37886eb6341bd1
laPepita <fgia1@student.monash.edu>
2016-06-05 11:25:54 +1000

    Reformatted the home page in the wagtail template to include the correct forms with basic validation (As restraints) Still need to add the Post method to pass info to the database.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -11,13 +11,13 @@ from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
-
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
     url(r'^search/$', search_views.search, name='search'),
-
     url(r'', include(wagtail_urls)),
+
+
+
 ]
 
 
